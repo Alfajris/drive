1a6e5dd0f7e9a5515b85967a93df23b8213074ed
Alfajri <77419836+Alfajris@users.noreply.github.com>
2022-11-02 11:25:35 +0700

    Update song_list.js

--- song_list.js
+++ song_list.js
@@ -1,40 +1,40 @@
 //song list
 let All_song = [
    {
-     name: "Make Me Move",
-     path: "music/1.mp3",
-     img: "images/1.jpg",
-     singer: "NoCopyrightSounds [NCS]"
+     name: "Centimeter",
+     path: "https://github.com/Alfajris/Bot-WA/raw/main/RentaGirlfriend%20%20Opening%20FullCentimeterby%20the%20peggies.mp3",
+     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhF-a6x9p-0bfGpNfvqnVJTlh4UgkNHOweOCsVTxTzYaSOEJfNYyQ1-P7B4_xTpKyyH3_crtao408skAsCasCX49kV4aXAKcD5RflT89FshcIlO77h4e-Dp0dcvNBsMH0uhBhzXVT2A3y7iSLnwLBc02_-KCGmDblexDDiOU1ykzCpBB-svh81aULmx/s318/Kanojo%20Oka.jpg",
+     singer: "The Peggies"
    },
    {
-     name: "Beauz & Jvna - Crazy",
-     path: "music/2.mp3",
-     img: "images/2.jpg",
-     singer: "NoCopyrightSounds"
+     name: "Nakanoke di Itsutsugo",
+     path: "https://github.com/Alfajris/Bot-WA/raw/main/Gotoubun%20no%20katachi%20Lyrics%20%20Gotoubun%20No%20Hanayome%20Season%202%20OP.mp3",
+     img: "https://cdn.myanimelist.net/images/anime/1819/97947.jpg",
+     singer: "Gotoubun No Hanayome"
    },
    {
-     name: "Skydive -Loxbeats",
-     path: "music/3.mp3",
-     img: "images/3.jpg",
-     singer: "Audio Library"
+     name: "Crossing Field",
+     path: "https://github.com/Alfajris/Bot-WA/raw/main/LiSA-Crossing%20Field%20Sword%20Art%20Online.mp3",
+     img: "https://cdn.myanimelist.net/images/anime/11/39717.jpg",
+     singer: "LiSA"
    },
    {
-     name: "Shahed -Indian Fusion",
-     path: "music/4.mp3",
-     img: "images/4.jpg",
-     singer: "imshahed"
+     name: "No Friends",
+     path: "https://github.com/Alfajris/Bot-WA/raw/main/Nightcore%20%20Stick%20Together.mp3",
+     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhNcfLpnKcIP6puzCtDHaTZWq6WazzFdlllv4-yjJf6gR_qpmz2RmAhOPjs5B6KuIdpwb0ITCT0H5U7Vj3Yp2BKyjcu40VbJ5lAYh80jK0BtHgj6Sc3rjMXJFzXnnc8CaTCv0MOns7hkCYdjobpQfKwcB0YBUZNgvOt98FIhapsV_08SoOkz2pbvp_D/s900/no%20friends.jpg",
+     singer: "Nightcore"
    },
    {
-     name: "Syn Cole - Feel Good",
-     path: "music/5.mp3",
-     img: "images/5.jpg",
-     singer: "NoCopyrightSounds"
+     name: "Stick Together",
+     path: "https://github.com/Alfajris/Bot-WA/raw/main/Nightcore%20%20No%20Friends%20Lyrics.mp3",
+     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7TqpL62Nb8AA4OqcFRLDT1TmSDuxAwbgahAQ3ApJ5W_sKswUKVaVLTVPPTjDx2cdFZhYTIZcK4LFYDc8O_ASB7IFx5piT0nkIljI4MdZEWosR8u86ttjfZEPsrH3XWCCaBlwBIj6F475mmqMixlcvdbk2KHxUxeyoT9nw3X6AOtPCCottpUFaoTI8/s728/2.jpg",
+     singer: "Nightcore"
    },
    {
-    name: "Yusuf Alev - Yoros",
-    path: "music/6.mp3",
-    img: "images/1.jpg",
-    singer: "Magic Free Release"
+    name: "Marry Me",
+    path: "https://github.com/Alfajris/Bot-WA/raw/main/Nightcore%20%20Marry%20Me.mp3",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiI6oPa6uNO8VNn3NwRqC118zg1ZG8QXW6UzrllbcM1zksjwRjQIGgNuMZ_50zOmx_WG0P2-idK9Pjq_qj6F-QHSx3eEm2xMf7gWxpD-pXKyPry6USD8_T9CWzmrNZZ1mNTYvuPh2DM_rkCSThsW-zGDfmDeQXq1ntD7yGlsRUhIp5G5TwrUS3zcwLH/s1920/3.jpg",
+    singer: "Nightcore"
    }
 ];
 /*you can add more song & images from you computer*/
